da771c9037caf3229f6b49779cc0a171be0631e6
Michael Kane <michael.kane@codegent.net>
2014-01-21 15:58:34 +0000

    bunch more stuff

--- addtopocket.py
+++ addtopocket.py
@@ -4,20 +4,28 @@ from __future__ import unicode_literals
 import os, re
 import urllib, urlparse
 import logging
+from datetime import datetime
 
-from flask import Flask, request, session, g, redirect, url_for
+from flask import (
+    Flask, request, session, g, redirect, url_for, render_template, flash
+)
+from flask.ext.sqlalchemy import SQLAlchemy
 import requests
+from bs4 import BeautifulSoup
+from dateutil import parser as date_parser
 
-app = Flask(__name__)
+###### Logging ######
 
 logging.basicConfig(level=logging.DEBUG)
 _logger = logging.getLogger()
 
-
 ###### Config ######
 
-def load_dotenv_as_environ_defaults(path):
-    '''Hepler to export any locally defined .env file as environment vars.'''
+def _load_dotenv_as_environ_defaults(path):
+    '''Hepler to export any locally defined .env file as environment vars.
+
+    This allows compatibily between running on Heroku and in dev.
+    '''
     try:
         with open(path) as f:
             content = f.read()
@@ -37,42 +45,38 @@ def load_dotenv_as_environ_defaults(path):
             _logger.debug("Setting env default: %s = %s" % (key, val))
             os.environ.setdefault(key, val)
 
-load_dotenv_as_environ_defaults(os.path.join(os.path.dirname(__file__), '.env'))
+_load_dotenv_as_environ_defaults(os.path.join(os.path.dirname(__file__), '.env'))
 
 # Read environ vars (for use with heroku)
-app.config.setdefault('SECRET_KEY', os.environ.get('SECRET_KEY'))
-app.config.setdefault('SQLALCHEMY_DATABASE_URI', os.environ.get('SQLALCHEMY_DATABASE_URI'))
-app.config.setdefault('POCKET_APP_CONSUMER_KEY', os.environ.get('POCKET_APP_CONSUMER_KEY'))
+_config = {
+    'SECRET_KEY': os.environ.get('SECRET_KEY'),
+    'SQLALCHEMY_DATABASE_URI': os.environ.get('SQLALCHEMY_DATABASE_URI'),
+    'POCKET_APP_CONSUMER_KEY': os.environ.get('POCKET_APP_CONSUMER_KEY')
+}
+
+
+###### Init ######
+
+app = Flask(__name__)
+app.config.update(_config)
+db = SQLAlchemy(app)
 
 
 ###### Basic Setup ######
 
-# @bp.teardown_request
-# def shutdown_session(exception=None):
-#     '''Ensure the database connection is released'''
-#     db_session.remove()
+@app.before_request
+def set_current_user(user=None):
+    g.user = None
 
-# @bp.before_request
-# def set_current_user(user=None):
-#     g.user = None
-
-#     if user:
-#         session['user_id'] = user.id
-#         g.user = user
-
-#     if 'user_id' in session:
-#         g.user = Person.query.get(session['user_id'])
+    if user:
+        session['user_id'] = user.id
+        g.user = user
+    
+    elif session.get('user_id', None):
+        g.user = Person.query.get(session['user_id'])
 
 
 ###### Helpers ######
-
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if g.user is None:
-            return redirect(url_for('.login'))
-        return f(*args, **kwargs)
-    return decorated_function
 
 POCKET_API_BASE = 'https://getpocket.com/v3/'
 
@@ -87,8 +91,35 @@ def pocket_request(path, data=None):
 
     return requests.post(url, data, headers=headers)
 
+AEON_FEED_URL = 'http://feeds.feedburner.com/AeonMagazineEssays'
 
-###### Login / Logout / Signup ######
+def get_aeon_rss_soup():
+    return BeautifulSoup(requests.get(AEON_FEED_URL).text)
+
+def import_latest_aeon_articles():
+    # Retreive the feed
+    soup = get_aeon_rss_soup()
+
+    # Iterate over each item in the feed
+    for item in soup.find_all('item'):
+        # If we already know about this article we can ignore it
+        if Article.query.filter_by(guid=item.guid.text).count():
+            continue
+
+        _logger.info('Importing Article: %s' % item.text)
+
+        article = Article()
+        article.guid = item.guid.text
+        article.title = item.title.text
+        article.link = item.link.text
+        article.pubdate = date_parser.parse(item.pubdate.text)
+        article.raw_feed = item.text
+        db.session.add(article)
+
+    db.session.commit()
+
+
+###### Pages ######
 
 @app.route('/login')
 def login():
@@ -111,6 +142,7 @@ def login():
 
     return redirect(authentication_url)
 
+
 @app.route('/oauth-login-callback')
 def oauth_login_callback():
     # When we get here, there should be a request token in the users session for
@@ -121,14 +153,35 @@ def oauth_login_callback():
             'consumer_key': app.config['POCKET_APP_CONSUMER_KEY'],
             'code': session.pop('request_token')
     })
-    response_data = request_token_response.json()
 
-    access_token = response_data['access_token']
-    username = response_data['username']
+    # Did they actually sign in and authorize us?  If not, get them back to the
+    # home page -- but log them out first, just in case.
+    if not (access_token_response.ok):
+        return redirect(url_for('.logout'))
 
-    _logger.debug(response_data)
+    response_data = access_token_response.json()
+    access_token = response_data.get('access_token', None)
+    username = response_data.get('username', None)
+
+    # Try to find the user
+    user = Person.query.filter_by(username=username).first()
+
+    # Create new user if they're new
+    if not user:
+        user = Person()
+        user.username = username
+        db.session.add(user)
+        _logger.info('New User: %s' % user.username)
+
+    # Ensure we have their latest access_token
+    user.access_token = access_token
+    db.session.commit()
+    
+    # And log them in
+    set_current_user(user)
     
     return redirect(url_for('.home'))
+
 
 @app.route('/logout')
 def logout():
@@ -137,12 +190,48 @@ def logout():
     return redirect(url_for('.home'))
 
 
-###### Pages ######
-
 @app.route('/')
 def home():
-    return 'Hi there!' #render_template('home.html')
+    articles = None
+    if g.user:
+        articles = Article.query.order_by('pubdate DESC').all()
 
+    return render_template('home.html', articles=articles)
+
+
+@app.route('/import-latest-articles')
+def import_latest_articles():
+    old_count = Article.query.count()
+    import_latest_aeon_articles()
+    new_count = Article.query.count() - old_count
+    flash('%s new articles found' % new_count, 'info')
+    return redirect(url_for('.home'))
+
+
+
+###### Models ######
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    access_token = db.Column(db.String, nullable=False)
+    status = db.Column(db.String, default='active')
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, 
+                           nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, 
+                           onupdate=datetime.utcnow, nullable=False)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    guid = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    link = db.Column(db.String, nullable=False)
+    pubdate = db.Column(db.DateTime, nullable=False)
+    raw_feed = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, 
+                           nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, 
+                           onupdate=datetime.utcnow, nullable=False)
 
 
 ###### Run Dev ######
